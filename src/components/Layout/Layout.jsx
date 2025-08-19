5b68ec7633e9318fec71c78040b0b2ca7d85ac21
Alain Daly <alain.daly.93600@gmail.com>
2025-08-19 16:35:52 +0200

    fix: consistent Sidebar path casing and imports

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Header from "../Header/Header";
-import Sidebar from "../SideBar/Sidebar";
+import Sidebar from "../Sidebar/Sidebar.jsx";
 import styles from './Layout.module.scss';
 
 const Layout = () => {
